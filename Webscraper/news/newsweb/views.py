92e96176ec224e6c0e7f0428ccb73845cfabf8ac
beccaburgmeier <becca.burgmeier@gmail.com>
2020-12-02 17:46:41 +0100

    update

---
+++ Webscraper/news/newsweb/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    import requests
+    import json
+
+    news_api_request = requests.get('http://newsapi.org/v2/everything?'
+                                    'q="Black Lives Matter"&'
+                                    'from=2020-12-2&'
+                                    'sortBy=relevancy&'
+                                    'apiKey=2adab610c29b44e8bdfdcdcd8a428df3')
+    api = json.loads(news_api_request.content)
+    return render(request, 'home.html', {'api': api})
